7d66f151a59680a2ba19b3a53a7db4099964b24b
sategie <>
2023-09-21 11:30:46 +0000

    Modify PostAdmin class in admin.py to personalise the Post model in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,9 @@ from .models import Post
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'slug', 'status', 'created_date')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
 
 admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
 
 
 class Post(models.Model):
@@ -17,4 +23,10 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     post_content = models.TextField()
-    status = models.IntegerField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
