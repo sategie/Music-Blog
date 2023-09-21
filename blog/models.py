62597d5c46e6c5a31ec13cc444e4a5d3d0c8c8b8
sategie <>
2023-09-21 11:11:29 +0000

    Edit the Post model with some initial code and register the model under admin.py

--- blog/models.py
+++ blog/models.py
@@ -7,12 +7,12 @@ class Post(models.Model):
     """
     A Post class which subclasses from django.db.models.Model
 
-    Takes in a variety of attributes which make up the Database columns for 
+    Takes in a variety of attributes which make up the Database columns for
     the Post entity
     """
     post_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique_for_date='modified_date')
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
